2f1cf9b1d8bd69a5800cfdcd1c4046cd3a9153bb
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-09-01 14:04:01 -0300

    usando group para aplicar estilização com base no estado dco componente pai

--- src/app/components/mainNavigation/index.tsx
+++ src/app/components/mainNavigation/index.tsx
@@ -5,10 +5,12 @@ export default function Navigation() {
     <nav className="space-y-05 ">
       <a
         href=""
-        className="flex items-center gap-3 rounded px-3 py-2 hover:bg-violet-50 "
+        className="flex items-center gap-3 rounded px-3 py-2 hover:bg-violet-50 group"
       >
         <Home className="h-5 w-5" />
-        <span className="font-medium text-zinc-700 ">Home</span>
+        <span className="font-medium text-zinc-700 group-hover:text-violet-500">
+          Home
+        </span>
         <ChevronDown className="ml-auto w-5 h-5 text-zinc-400" />
       </a>
       <a href=""></a>
