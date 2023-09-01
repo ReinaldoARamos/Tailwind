d5ce330a257f942e13a21bcd0c42119d2b969f29
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-09-01 15:36:06 -0300

    criando o widget de espaço e alguns outros items da sidebar

--- src/app/components/sidebar/index.tsx
+++ src/app/components/sidebar/index.tsx
@@ -1,10 +1,22 @@
-import { Search } from 'lucide-react'
+import {
+  BarChart2,
+  Check,
+  Flag,
+  Home,
+  Search,
+  SquareSlash,
+  Users,
+  LifeBuoy,
+  Cog,
+} from 'lucide-react'
 import LogoMark from './Logo'
-import Navigation from '../mainNavigation'
+
+import { NavItem } from './NavItem'
+import { UsedSpaceWidget } from '../UsedSpaceWidget'
 
 export default function Sidebar() {
   return (
-    <aside className="border-r border-zinc-200 px-5 py-8  space-y-6">
+    <aside className="border-r border-zinc-200 px-5 py-8  flex flex-col gap-6 ">
       <LogoMark />
       <div className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
         <Search className="h-5 w-5 text-zinc-500" />
@@ -13,7 +25,23 @@ export default function Sidebar() {
           placeholder="Search"
         />
       </div>
-      <Navigation />
+
+      <nav className="space-y-05">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart2} />
+        <NavItem title="Projects" icon={SquareSlash} />
+        <NavItem title="Tasks" icon={Check} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+        <UsedSpaceWidget />
+      </div>
     </aside>
   )
 }
