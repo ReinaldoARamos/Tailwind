5902d8f34a40878c9a9690afe9a97f03f9745521
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-09-06 15:41:32 -0300

    pegando o valor da URL e criando o preview

--- src/app/components/Form/FileInput/ImagePreview.tsx
+++ src/app/components/Form/FileInput/ImagePreview.tsx
@@ -1,9 +1,33 @@
+'use client'
 import { User } from 'lucide-react'
+import { useFileInput } from './Root'
+import { useMemo } from 'react'
 
 export function ImagePreview() {
-  return (
-    <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full">
-      <User className="w-8 h-8 text-zinc-500" />
-    </div>
-  )
+  const { files } = useFileInput()
+
+  const previewUrl = useMemo(() => {
+    if (files.length === 0) {
+      return null
+    } else {
+      return URL.createObjectURL(files[0])
+    }
+  }, [files])
+  if (previewUrl === null) {
+    return (
+      <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full">
+        <User className="w-8 h-8 text-zinc-500" />
+      </div>
+    )
+  } else {
+    return (
+      <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full">
+        <img
+          src={previewUrl}
+          alt=""
+          className="h-16 w-16 rounded-full object-cover"
+        />
+      </div>
+    )
+  }
 }
