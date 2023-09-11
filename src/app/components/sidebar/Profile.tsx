d40c1a80c1060bd460905feb5e66a0990b8580ba
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-09-11 09:18:54 -0300

    ajustando a imagem do profile

--- src/app/components/sidebar/Profile.tsx
+++ src/app/components/sidebar/Profile.tsx
@@ -4,14 +4,19 @@ export function Profile() {
   return (
     /* eslint-disable */
     <div className="grid items-center gap-3 grid-cols-profile">
+      
+      <div className="flex flex-1 gap-3 truncate">
       <img
         src="https://avatars.githubusercontent.com/u/55931337?v=4"
         alt=""
         className="h-10 w-10 rounded-full"
       />
-      <div className="flex flex-1 flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">Reinaldo</span>
+      <div className='flex flex-1 flex-col truncate'>
+      <span className="text-sm font-semibold text-zinc-700">Reinaldo</span>
+        
         <span className='text-sm text-zinc-500 '>Gamersolitavi4l@gmail.com</span>
+      </div>
+      
  
         
       </div>
