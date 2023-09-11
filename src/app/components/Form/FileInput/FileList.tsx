6f678cafe87d3a7bc58443d20be97ba260a229fb
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-09-11 11:12:51 -0300

    usando o auto animate

--- src/app/components/Form/FileInput/FileList.tsx
+++ src/app/components/Form/FileInput/FileList.tsx
@@ -1,13 +1,13 @@
 'use client'
-
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { UploadCloud } from 'lucide-react'
 import { useFileInput } from './Root'
 import { formatBytes } from '../../../utils/FormatBytesToMb'
 export function FileList() {
   const { files } = useFileInput()
-
+  const [parent] = useAutoAnimate()
   return (
-    <div className="mt-4 space-y-3">
+    <div ref={parent} className="mt-4 space-y-3">
       {files.map((file) => {
         return (
           <div
